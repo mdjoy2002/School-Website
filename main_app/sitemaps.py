d4d9c6f548d939aadccb73635881dadd902108e7
mdjoy2002 <mdimtiazsarderjoy@gmail.com>
2026-05-11 01:34:31 +0600

    Added Google Search Console Meta Tag

--- main_app/sitemaps.py
+++ main_app/sitemaps.py
@@ -5,16 +5,27 @@ from django.contrib.sites.models import Site
 class StaticViewSitemap(Sitemap):
     priority = 0.5
     changefreq = 'daily'
-    protocol = 'https'
+    protocol = 'https'  # লিঙ্কগুলোকে https হিসেবে জেনারেট করবে
 
     def items(self):
-        return ['home', 'all_notices', 'exam_routine', 'teachers_page', 'gallery', 'contact']
+        # আপনার urls.py-তে যে নামগুলো (name='') দেওয়া আছে সেগুলো এখানে লিস্ট করা হয়েছে
+        return [
+            'home', 
+            'all_notices', 
+            'exam_routine', 
+            'teachers_page', 
+            'gallery', 
+            'contact'
+        ]
 
     def location(self, item):
+        # এটি প্রতিটি নামের জন্য সঠিক URL পাথ তৈরি করবে
         return reverse(item)
 
-    # এই অংশটি সাইটম্যাপে example.com এর বদলে আপনার ডোমেইন বসিয়ে দিবে
     def get_urls(self, site=None, **kwargs):
-        # ডাটাবেজ চেক না করে সরাসরি আপনার সাইট অবজেক্ট তৈরি করে দেওয়া
-        site = Site(domain='knuhighschool.pythonanywhere.com', name='Khandaker Naser Uddin High School')
+        # ডাটাবেজের সেটিংসের ওপর নির্ভর না করে সরাসরি আপনার ডোমেইনটি বসিয়ে দেওয়ার জন্য এই অংশটি
+        site = Site(
+            domain='knuhighschool.pythonanywhere.com', 
+            name='Khandaker Naser Uddin High School'
+        )
         return super().get_urls(site=site, **kwargs)
