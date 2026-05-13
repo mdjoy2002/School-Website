0b140dc9c6ec4f3f8065899b25379571d15bd9fb
mdjoy2002 <mdimtiazsarderjoy@gmail.com>
2026-05-13 16:34:53 +0600

    Added Google Search Console Meta Tag

--- main_app/admin.py
+++ main_app/admin.py
@@ -9,6 +9,7 @@ from .models import (
 class TickerNewsAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_active', 'created_at')
     list_filter = ('is_active',)
+    search_fields = ('title',)
 
 # --- নোটিশ বোর্ড (Notice Management) সেকশন ---
 @admin.register(Notice)
