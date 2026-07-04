fd446560acd7a2a49266de6032f091221bbb12af
mdjoy2002 <mdimtiazsarderjoy@gmail.com>
2026-07-04 07:14:44 +0600

    Add final migration reconciliation for teacher signature

---
+++ main_app/migrations/0033_reconcile_teacher_signature_migration.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main_app', '0032_merge_20260704_0000'),
+    ]
+
+    operations = []
